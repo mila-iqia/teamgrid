123ed8e1d6155b889339e5f74b8e322ec27499ff
JulienRoy <jul.roy1311@gmail.com>
2019-05-10 14:18:33 -0400

    TMP: reverted change rendering

--- teamgrid/minigrid.py
+++ teamgrid/minigrid.py
@@ -1345,7 +1345,7 @@ class MiniGridEnv(gym.Env):
 
         r.endFrame()
 
-        return r.getArray()
+        return r.getPixmap()
 
     def render(self, mode='human', close=False):
         """
