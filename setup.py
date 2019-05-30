ac11ab5a5417b8bb3c651262487aed27daee67db
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-05-30 13:57:36 -0400

    Fix error in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='teamgrid',
     version='0.0.1',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    packages=['teamgrid', 'teamgrid.envs', 'teamgrid.wrappers'],
+    packages=['teamgrid', 'teamgrid.envs'],
     install_requires=[
         'gym>=0.9.6',
         'numpy>=1.15.0',
